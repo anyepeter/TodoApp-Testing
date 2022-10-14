ae6af208e9ba76e140b7f15d465a51e635724aa6
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-10-14 17:03:17 +0100

    Create the test case for update function

--- addremoveItems.test.js
+++ addremoveItems.test.js
@@ -1,4 +1,4 @@
-import { addItems, renderItems } from './src/modules/addremoveItems.js';
+import { addItems, renderItems, checkItemsUpdate  } from './src/modules/addremoveItems.js';
 
 describe('Add and Remove Testing', () => {
   document.body.innerHTML = `
@@ -22,4 +22,32 @@ describe('Add and Remove Testing', () => {
     const itemsToDo = document.getElementById('containerItems');
     expect(itemsToDo.childElementCount).toBe(1);
   });
+});
+
+
+describe('Testing the update functionalities', () => {
+  document.body.innerHTML = `
+    <input id='toDoInput'>
+    <button id='addBtn'></button>
+    <div id='containerItems'></div>
+    `;
+
+  test('Should update all the completed to true', () => {
+    const localGet = JSON.parse(localStorage.getItem('listStorage'));
+    localGet[0].completed = true;
+    localStorage.setItem('listStorage', JSON.stringify(localGet));
+    renderItems();
+    const itemsToDo = document.getElementById('containerItems');
+    expect(itemsToDo.children[0].children[0].checked).toBe(true);
+  });
+
+  test('Should update  all the completed to false', () => {
+    const localGet = JSON.parse(localStorage.getItem('listStorage'));
+    localGet[0].completed = false;
+    localStorage.setItem('listStorage', JSON.stringify(localGet));
+    renderItems();
+    const itemsToDo = document.getElementById('containerItems');
+    expect(itemsToDo.children[0].children[0].checked).toBe(false);
+  });
+
 });
